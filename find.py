e0f338db5c6b151810b6e994815e5802e4a9dc6d
Soutif Thomas <th.soutif@gmail.com>
2024-06-23 13:32:34 +0200

    remove space line

--- find.py
+++ find.py
@@ -58,10 +58,8 @@ def capture_top_left_screen_region():
 def process_image(image):
     # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
     # Apply thresholding to preprocess the image
     _, binary_img = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
-
     # Perform OCR
     text = pytesseract.image_to_string(binary_img)
     return text
